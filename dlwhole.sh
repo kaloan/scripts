9aabc2a981a41773d0933808073a4abade0043db
Kaloian Stoilov <kkstoilov@uni-sofia.bg>
2022-10-26 08:27:16 +0300

    Improve generic download

--- dlwhole.sh
+++ dlwhole.sh
@@ -1,6 +1,16 @@
 #/bin/bash
+while getopts "d:" arg; do
+  case $arg in
+    d)
+      mkdir "${OPTARG}"
+      cd "${OPTARG}"
+      shift
+      shift
+      ;;
+  esac
+done
 
-for j in $(seq $2 $3) 
+for j in $(seq $2 $3 $4) 
 do
-  wget "$1$j$4" 
+  wget "$1$j$5" -q -o /dev/null & 
 done
